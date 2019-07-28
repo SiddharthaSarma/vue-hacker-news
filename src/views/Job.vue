3e16d581d1739ab490ee616be829951d202c35f9
siddharthasarma <sidduinevitable@gmail.com>
2019-07-28 15:31:45 +0530

    add functionality to fetch data for other routes :sparkles:

---
+++ src/views/Job.vue
@@ -0,0 +1,36 @@
+<template>
+  <section>
+    <ul class="list-group">
+      <li
+        class="list-group-item d-flex justify-content-between align-items-center"
+        v-for="list in newsList"
+        :key="list.id"
+      >
+        <a :href="list.url" target="_blank">{{list.title}}</a>
+        <span class="badge badge-primary badge-pill">{{list.comments_count}}</span>
+      </li>
+    </ul>
+  </section>
+</template>
+
+<script>
+import { value, onMounted } from 'vue-function-api';
+
+export default {
+  name: 'Job',
+  setup() {
+    const newsList = value([]);
+    onMounted(async () => {
+      const results = await fetch('https://api.hackernews.io/jobs?page=1');
+      const news = await results.json();
+      newsList.value = news;
+    });
+    return {
+      newsList,
+    };
+  },
+};
+</script>
+
+<style>
+</style>
